6b8e9f9211a222023dd1f9678f80031ffe00f7e2
ridhorambu93 <ridhodavybones@gmail.com>
2025-06-16 03:32:42 +0700

    add pagination & invoices pages

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,19 +1,38 @@
-export default function page() {
+import Pagination from "@/app/ui/invoices/pagination"
+import Search from "@/app/ui/search"
+import Table from "@/app/ui/invoices/table"
+import { CreateInvoice } from "@/app/ui/invoices/buttons"
+import { lusitana } from "@/app/ui/fonts"
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons"
+import { Suspense } from "react"
+import { fetchInvoicesPages } from "@/app/lib/data"
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string
+    page?: string
+  }>
+}) {
+  const searchParams = await props.searchParams
+  const query = searchParams?.query || ""
+  const currentPage = Number(searchParams?.page) || 1
+  const totalPages = await fetchInvoicesPages(query)
+  // const totalPages = 10 // Placeholder for total pages, replace with actual logic
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* Add your logo component here */}
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
-            Welcome to the Invoices Page.
-          </p>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-        </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
       </div>
-    </main>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
   )
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,53 @@
-'use client';
+"use client"
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline"
+import { useSearchParams, usePathname, useRouter } from "next/navigation"
+import { useDebouncedCallback } from "use-debounce"
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  // useSearchParams is a hook that returns the current URL search parameters
+  // usePathname is a hook that returns the current URL pathname
+  // useRouter is a hook that provides methods to manipulate the browser history
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  // useDebouncedCallback is a hook that returns a debounced version of the provided function
+  // The debounced function will delay the execution of the search handler by 300 milliseconds
+  // This is useful to avoid making too many requests while the user is typing in the search input field
+  // The handleSearch function will be called with the search term as an argument
+  // and will update the URL search parameters accordingly
+
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`)
+
+    const params = new URLSearchParams(searchParams)
+    params.set("page", "1") // Reset to page 1 on new search
+    // This ensures that the search results are always shown from the first page when a new search is performed
+    console.log("Current search params:", params.toString())
+    // If the term is not empty, set the "query" parameter to the term
+    if (term) {
+      params.set("query", term)
+    } else {
+      params.delete("query")
+    }
+    replace(`${pathname}?${params.toString()}`)
+  }, 300)
+
+  // function handleSearch(term: string) {
+  //   console.log("Searching for:", term)
+  //   const params = new URLSearchParams(searchParams)
+  //   if (term) {
+  //     params.set("query", term)
+  //   } else {
+  //     params.delete("query")
+  //   }
+  //   // Update the URL with the new search term
+  //   console.log("Updated search params:", params.toString())
+  //   // Use replace to avoid adding a new entry in the history stack
+  //   replace(`${pathname}?${params.toString()}`)
+  // }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,8 +56,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
-  );
+  )
 }
