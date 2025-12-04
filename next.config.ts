51b0904a3472fa98d28bee1147b3166c40cf3f8c
Muhammad Ridho <55222870+ridhorambu93@users.noreply.github.com>
2025-12-05 00:19:44 +0700

    Modify experimental options in next.config.ts
    Updated experimental configuration to include outputFileTracingRoot.

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,13 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
+      outputFileTracingRoot: join(__dirname, '../../'),
+   },
+/* old code  
+experimental: {
     ppr: "incremental",
   },
+  */
 }
 
 export default nextConfig
